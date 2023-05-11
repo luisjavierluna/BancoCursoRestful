9d591445e3ce5db4c50ebf37f9c86bbaab1f0073
luisjavierluna <javierlunaluis@gmail.com>
2023-05-10 21:33:23 -0600

    12 - Versionando y Creando Controlador Cliente

--- Application/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommand.cs
+++ Application/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommand.cs
@@ -1,4 +1,7 @@
-﻿using Application.Wrappers;
+﻿using Application.Interfaces;
+using Application.Wrappers;
+using AutoMapper;
+using Domain.Entities;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -20,9 +23,21 @@ namespace Application.Features.Clientes.Commands.CreateClienteCommand
 
     public class CreateClienteCommandHandler : IRequestHandler<CreateClienteCommand, Response<int>>
     {
+        private readonly IRepositoryAsync<Cliente> _repositoryAsync;
+        private readonly IMapper _mapper;
+
+        public CreateClienteCommandHandler(IRepositoryAsync<Cliente> repositoryAsync, IMapper mapper)
+        {
+            _repositoryAsync = repositoryAsync;
+            _mapper = mapper;
+        }
+
         public async Task<Response<int>> Handle(CreateClienteCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var nuevoRegistro = _mapper.Map<Cliente>(request);
+            var data = await _repositoryAsync.AddAsync(nuevoRegistro);
+
+            return new Response<int>(data.Id);
         }
     }
 }
