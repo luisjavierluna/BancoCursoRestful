2b425dcac66991329e3c69afe27337c953e3a76c
luisjavierluna <javierlunaluis@gmail.com>
2023-05-12 18:33:04 -0600

    14 - Actualizando y Eliminando Registros

--- WebAPI/Controllers/v1/ClientesController.cs
+++ WebAPI/Controllers/v1/ClientesController.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.Clientes.Commands.CreateClienteCommand;
+using Application.Features.Clientes.Commands.DeleteClienteCommand;
+using Application.Features.Clientes.Commands.UpdateClienteCommand;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +14,23 @@ namespace WebAPI.Controllers.v1
         public async Task<IActionResult> Post(CreateClienteCommand command)
         {
             return Ok(await Mediator.Send(command));
+        }
+
+        //PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, UpdateClienteCommand command)
+        {
+            if (id != command.Id)
+                return BadRequest();
+
+            return Ok(await Mediator.Send(command));
+        }
+
+        //DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await Mediator.Send(new DeleteClienteCommand { Id = id }));
         }
     }
 }
